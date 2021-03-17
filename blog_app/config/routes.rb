2ef9dd8206fe9497829302fca06fb7b70549b141
stephbegle <stephbegle@gmail.com>
2021-03-17 16:52:51 -0700

    almost finished all challengeswith an error in our update portion

--- blog_app/config/routes.rb
+++ blog_app/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'index'=>'post#index', as: 'posts'
-  get 'post/new'=>'post#new', as: 'new_post'
-  get 'post/:id'=>'post#show', as: 'post'
+  get 'posts'=>'post#index', as: 'posts'
+  get 'posts/new'=>'post#new', as: 'new_post'
+  get 'posts/:id'=>'post#show', as: 'post'
+  post 'posts' => 'post#create'
+  delete 'posts/:id' => 'post#destroy', as: 'delete_post'
+  get 'posts/:id/:edit' => 'post#edit', as: 'edit_post'
+  patch 'posts/:id' => 'post#update'
 end
